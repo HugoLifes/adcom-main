4fc985180f2c65ced6acff77fe82324506af9091
Hugolifes <rach.rc36@gmail.com>
2021-12-02 16:07:45 -0700

    chequeo de conexion

---
+++ lib/src/methods/checkInternet.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
+import 'package:flutter/material.dart';
+
+class CheckInternet {
+  StreamSubscription<DataConnectionStatus>? listener;
+  var InternetStatus = "Unknown";
+  var contentmessage = "Unknown";
+
+  checkConnection(BuildContext context) async {
+    print("Current status: ${await DataConnectionChecker().connectionStatus}");
+
+    listener = DataConnectionChecker().onStatusChange.listen((status) {
+      switch (status) {
+        case DataConnectionStatus.connected:
+          InternetStatus = "Whoop! Estas conectado a internet";
+          contentmessage = "Tienes conexion, ahora puedes navegar";
+          _showDialog(InternetStatus, contentmessage, context);
+          break;
+        case DataConnectionStatus.disconnected:
+          InternetStatus = "Oh no! No tienes conexion a internet";
+          contentmessage = "Porfavor conectate a internet para navegar";
+          _showDialog(InternetStatus, contentmessage, context);
+          break;
+      }
+    });
+    return await DataConnectionChecker().connectionStatus;
+  }
+
+  void _showDialog(String title, String content, BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              title: new Text(title),
+              content: new Text(content),
+              actions: <Widget>[
+                new TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: new Text("Close"))
+              ]);
+        });
+  }
+}
